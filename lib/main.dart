480fb1c4abba202d329b0c66af2bf309c4a6c3cd
Shripad Rao <shripada0987@gmail.com>
2024-04-01 14:58:48 +0530

    restructure using routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:lexichat/screens/llm_setup.dart';
+import 'package:lexichat/screens/home.dart';
+import 'package:lexichat/screens/loading.dart';
 import 'package:lexichat/screens/signup.dart';
-import 'package:lexichat/utils/jwt.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:lexichat/screens/welcome.dart';
 import 'firebase_options.dart';
-import 'package:http/http.dart' as http;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,96 +16,29 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String? jwtToken;
-  bool isBackendConnected = false;
-
-  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
-      GlobalKey<ScaffoldMessengerState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _getJwtToken();
-    _checkBackendConnection();
-  }
-
-  Future<void> _getJwtToken() async {
-    String token = await JwtUtil.getJwtToken();
-    print("token ${token}");
-    setState(() {
-      jwtToken = token;
-    });
-  }
-
-  Future<void> _checkBackendConnection() async {
-    bool isConnected = await checkBackendConnection();
-    setState(() {
-      isBackendConnected = isConnected;
-    });
-    _showBackendConnectionStatus();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your App',
+      title: "LexiChat",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        splashColor: Colors.transparent,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: Scaffold(
-        body: jwtToken == null
-            ? Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/app_logo.png'),
-                    SizedBox(height: 6),
-                    Text('Loading...'),
-                  ],
-                ),
-              )
-            : jwtToken != ""
-                ? LLMSetupScreen()
-                : SignUpScreen(),
-      ),
-      scaffoldMessengerKey: _scaffoldMessengerKey,
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        // '/chat': (context) => const ChatScreen(),
+        // '/message': (context) => const MessageScreen(),
+        // '/profile': (context) => const ProfileScreen(),
+        // '/login': (context) => const LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/loading': (context) => const LoadingScreen(),
+        '/home': (context) => HomeScreen(),
+      },
+      initialRoute: '/loading',
     );
-  }
-
-  void _showBackendConnectionStatus() {
-    final scaffoldMessenger = _scaffoldMessengerKey.currentState;
-    if (scaffoldMessenger != null) {
-      final message = isBackendConnected
-          ? 'Connected to backend'
-          : 'Unable to connect to backend';
-      scaffoldMessenger.showSnackBar(
-        SnackBar(
-          content: Text(message),
-          duration: Duration(seconds: 3),
-        ),
-      );
-    }
-  }
-}
-
-Future<bool> checkBackendConnection() async {
-  try {
-    final response =
-        await http.get(Uri.parse(dotenv.env["BASE_API_URL"]! + "/api/ping"));
-    print("response: ${response.body}");
-    return response.statusCode == 200;
-  } on SocketException catch (_) {
-    // Unable to connect to the backend server
-    return false;
-  } catch (e) {
-    // Other errors
-    print("Error: $e");
-    return false;
   }
 }
---
+++ lib/screens/loading.dart
@@ -0,0 +1,109 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lexichat/utils/jwt.dart';
+import 'package:http/http.dart' as http;
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  String? jwtToken;
+  bool isBackendConnected = false;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    await Future.wait([
+      _getJwtToken(),
+      _checkBackendConnection(),
+    ]);
+
+    if (jwtToken == null || jwtToken == "") {
+      Navigator.pushReplacementNamed(context, '/welcome');
+    } else {
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+  Future<void> _getJwtToken() async {
+    String? token = await JwtUtil.getJwtToken();
+    print("token $token");
+    setState(() {
+      jwtToken = token;
+    });
+  }
+
+  Future<void> _checkBackendConnection() async {
+    bool isConnected = await checkBackendConnection();
+    setState(() {
+      isBackendConnected = isConnected;
+    });
+    // _showBackendConnectionStatus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff121212),
+      body: Center(
+        child: Image.asset(
+          'assets/images/app_logo.png',
+          height: 200,
+          width: 200,
+        ),
+      ),
+    );
+  }
+
+  void _showBackendConnectionStatus() {
+    final scaffoldMessenger = _scaffoldMessengerKey.currentState;
+    if (scaffoldMessenger != null) {
+      final message = isBackendConnected
+          ? 'Connected to backend'
+          : 'Unable to connect to backend';
+      scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content: Text(message),
+          duration: const Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+}
+
+Future<bool> checkBackendConnection() async {
+  try {
+    String? baseApiUrl = dotenv.env["BASE_API_URL"];
+    if (baseApiUrl == null || baseApiUrl.isEmpty) {
+      print("BASE_API_URL not found or empty in the .env file");
+      return false;
+    }
+
+    final response = await http.get(Uri.parse(baseApiUrl + "/api/ping"));
+    print("response: ${response.body}");
+    return response.statusCode == 200;
+  } catch (e) {
+    if (e is SocketException) {
+      //treat SocketException
+      print("Socket exception: ${e.toString()}");
+    } else if (e is TimeoutException) {
+      //treat TimeoutException
+      print("Timeout exception: ${e.toString()}");
+    } else
+      print("Unhandled exception: ${e.toString()}");
+  }
+  return false;
+}
