0fbb5b92722557ecaa92d6336488eaa5d6f61b73
Shripad Rao <shripada0987@gmail.com>
2024-04-05 00:26:02 +0530

    changes and additions to User Model

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,23 +1,37 @@
 class User {
-  final String? id;
-  final String username;
+  final String userID;
+  final String userName;
   final String phoneNumber;
-  final List<int> profilePicture;
+  final String fcmToken;
+  final List<int>? profilePicture;
   final String? createdAt;
 
   User({
-    this.id,
-    required this.username,
+    required this.userID,
+    required this.userName,
     required this.phoneNumber,
+    required this.fcmToken,
     required this.profilePicture,
     this.createdAt,
   });
 
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'username': username,
+        'userid': userID,
+        'username': userName,
         'phone_number': phoneNumber,
+        'fcm_token': fcmToken,
         'profile_picture': profilePicture,
         if (createdAt != null) 'created_at': createdAt,
       };
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      userID: json['userid'],
+      userName: json['username'],
+      phoneNumber: json['phone_number'],
+      profilePicture: json['profile_picture'],
+      fcmToken: json['fcm_token'],
+      createdAt: json['created_at'],
+    );
+  }
 }
