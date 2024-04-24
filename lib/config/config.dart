876bae59986a56291f1eca2b1c716e266fc8dfcd
Shripad Rao <shripada0987@gmail.com>
2024-04-24 12:34:38 +0530

    add local user state

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lexichat/models/User.dart';
 
 late String FCMToken;
+late String JWT_Token;
 String? BASE_API_URL = dotenv.env["BASE_API_URL"];
+
+// user details
+late User userDetails;
